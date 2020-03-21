b36c9e08bc1f80f9deaf9e11435749aa38547edf
Margot Paez <vansgirl12@me.com>
2020-03-20 21:42:21 -0400

    re-enabling

--- js/gacovid19-map.js
+++ js/gacovid19-map.js
@@ -1,4 +1,4 @@
-Plotly.d3.csv('csv/gacounties.csv', function(err, rows){
+Plotly.d3.csv('https://www.margotbits.com/gacovid19/csv/gacounties.csv', function(err, rows){
 
     function unpack(rows, key) {
         return rows.map(function(row) { return row[key]; });
